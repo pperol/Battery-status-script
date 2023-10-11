9058d132b393e2b051021fa9521b7c5560562076
Pierre Perol-Schneider <94623429+pperol@users.noreply.github.com>
2023-10-11 20:59:52 +0200

    Update batt-stat.sh

--- batt-stat.sh
+++ batt-stat.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
-while true; do
-	export DISPLAY=:0
-	battery_level=`acpi -b | grep -Po '[0-9]+(?=%)'`
-	ac_power=`cat /sys/class/power_supply/ADP0/online`
-	if [ $ac_power -eq 0 ]; then
-		if [ $battery_level -le 20 ]; then
-			/usr/bin/notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}% "
-			/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
-		fi
-	elif  [ $ac_power -eq 1 ]; then
-		if [ $battery_level -ge 80 ]; then
-			/usr/bin/notify-send --urgency=CRITICAL --icon=/usr/share/icons/Adwaita/16x16/legacy/battery-good.png "Optimal battery level reached" "Level: ${battery_level}%"
-			/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/complete.oga
-		fi
-	else
-		/usr/bin/notify-send --urgency=CRITICAL "Cannot determine whether AC power is ON or OFF."
+export DISPLAY=:0
+battery_level=`acpi -b | head -1 | grep -Po '[0-9]+(?=%)'`
+ac_power=`cat /sys/class/power_supply/ADP0/online`
+if [ $ac_power -eq 0 ]; then
+	if [ $battery_level -le 20 ]; then
+		/usr/bin/notify-send --urgency=CRITICAL "Battery Low" "Level: ${battery_level}% "
+		/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga
 	fi
-	sleep 60
-done
+elif  [ $ac_power -eq 1 ]; then
+	if [ $battery_level -ge 85 ]; then
+		/usr/bin/notify-send --urgency=CRITICAL --icon=/usr/share/icons/Adwaita/16x16/legacy/battery-good.png "Optimal battery level reached" "Level: ${battery_level}%"
+		/usr/bin/paplay /usr/share/sounds/freedesktop/stereo/complete.oga
+	fi
+else
+	/usr/bin/notify-send --urgency=CRITICAL "Cannot determine whether AC power is ON or OFF."
+fi
